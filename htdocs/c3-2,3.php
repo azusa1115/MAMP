6de0879fcf4dfdae8a1d2430e8f81823401ea1e3
azusa1115 <kato.azusa.1115@gmail.com>
2021-03-03 00:30:58 +0900

    first commit

---
+++ htdocs/c3-2,3.php
@@ -0,0 +1,15 @@
+<?php
+// print((123+2)*5/3);
+// print("\n");
+// print(60*60*24);
+// print("\n");
+// print('ここから３−３');
+// print("\n");
+print('現在は'.date('G時 i分 s秒').'です');
+// print(date('G'));
+// print(date('i'));
+// print(date('s'));
+// print('現在は');
+// print(date('G時 i分 s秒'));
+// print('です');
+?>
---
+++ htdocs/c3.php
@@ -0,0 +1,9 @@
+<pre>
+<?php
+print("\n");
+print('一行目');
+print("\n");
+print('二行目');
+?>
+</pre>
+    
