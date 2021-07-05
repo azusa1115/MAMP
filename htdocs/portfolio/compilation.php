aa3b0621068d299b7f809e4fbe2a07cb06e207d3
azusa1115 <kato.azusa.1115@gmail.com>
2021-07-05 23:50:46 +0900

    登ページ編集

--- htdocs/portfolio/compilation.php
+++ htdocs/portfolio/compilation.php
@@ -6,7 +6,9 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>プラン比較サイト</title>
-<link rel="stylesheet" href="complation.css">
+<link rel="stylesheet" href="compilation.css">
 </head>
+<div>登録完了しました</div>
+<button type="button" onclick="location.href='main.php'">TOPに戻る</button>
 <body>
 </body>
